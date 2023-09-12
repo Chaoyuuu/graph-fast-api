268306037105f222971f5b02eccf4eafe8a8e35d
chaoyuuu <chaoyu2330@gmail.com>
2023-09-13 01:39:29 +0800

    replace to form-data

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI, Response
+from fastapi import FastAPI, Response, Form
 from typing import List
 from pydantic import BaseModel
+import json
 import openCV
 import KML
 
@@ -17,13 +18,13 @@ class Request(BaseModel):
 
 app = FastAPI()
 
-
-@app.get("/")
-async def root(request: Request):
+@app.post("/download-kml")
+async def get_object(name: str = Form(...), placeMarks: str = Form(...)):
+    json_data = json.loads(placeMarks)
     placeMarksKMLs = []
-    for index, placeMark in enumerate(request.placeMarks):
-        coordinates = openCV.runTrans(placeMark.EXT, placeMark.IMG)
-        placeMarksKMLs.append(KML.generatePlaceMark(f"{request.name}_{index}", coordinates))
+    for index, placeMark in enumerate(json_data):
+        coordinates = openCV.runTrans(placeMark["EXT"], placeMark["IMG"])
+        placeMarksKMLs.append(KML.generatePlaceMark(f"{name}_{index}", coordinates))
 
     kml = KML.generateKML(placeMarksKMLs)
     return Response(content=kml, status_code=200, media_type="application/xml")
